10c93e1d5b82bf31dfed1316fd47eda693da7042
YU LUO <yluo5432@uni.sydney.edu.au>
2023-06-05 16:25:20 +1000

    adding brew display with simple animation

--- public/accordion.js
+++ public/accordion.js
@@ -4,6 +4,22 @@ import 'accordion-js/dist/accordion.min.css';
 const config = {
     duration: 400,
     showMultiple: true,
-    collapse: true
+    collapse: true,
+    beforeOpen: (c) => {changeArrow(c)},
+    beforeClose: (c) => {changeArrow(c)}
 }
 new Accordion('.accordion-container', config);
+
+
+function changeArrow(element){
+    let button = element.children[0].children[0].children[0].children;
+    let arrow = button[button.length-1];
+    
+    if(arrow.classList.contains("down-arrow")){
+        arrow.classList.remove("down-arrow");
+        arrow.classList.add("up-arrow");
+    }else {
+        arrow.classList.add("down-arrow");
+        arrow.classList.remove("up-arrow");
+    }
+}
